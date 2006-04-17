e0c7ed567a2234cabdc955557a9ddb7794fdbdd6
flament <flament@eb54f2c9-7f0f-0410-ba04-aaec33d8da6a>
2006-04-17 12:07:45 +0000

    - Make it work with the current revision of the network directory - Some tests with the code of GuildAdsDTS:SendUpdateData
    git-svn-id: svn://svn.code.sf.net/p/guildads/code@68 eb54f2c9-7f0f-0410-ba04-aaec33d8da6a

--- GuildAds/data/testDatatypes.lua
+++ GuildAds/data/testDatatypes.lua
@@ -1,4 +1,15 @@
-GUILDADS_VERSION = 200;
+--
+-- Config
+--
+config = {
+	playerName = "Zarkan";
+	realmName = "Ner'zhul";
+	faction = "Horde";
+}
+
+--
+-- About WOW
+--
 SlashCmdList = {}
 tinsert = table.insert;
 getn = table.getn;
@@ -7,9 +18,36 @@ getglobal = function(name)
 	return _G[name];
 end
 
-GAS_currentTime = function()
-	return 4012;
+GetCVar = function(name)
+	if name=="realmName" then
+		return config.realmName;
+	end
 end
+
+UnitName = function(name)
+	if name=="player" then
+		return config.playerName;
+	end
+end
+
+--
+-- About GuildAds
+--
+GUILDADS_VERSION = 200;
+
+GuildAdsTask = {
+	AddNamedSchedule = function(self, n, t, r, c, f, ...)
+	end
+}
+
+GuildAds_ChatDebug = function(t, m)
+end
+
+AceEventFrame = {
+	RegisterEvent = function(event)
+	end;
+}
+
 
 dofile("C:\\Program Files\\World of Warcraft\\Interface\\AddOns\\Ace\\Ace.lua")
 dofile("C:\\Program Files\\World of Warcraft\\Interface\\AddOns\\Ace\\AceDB.lua")
@@ -19,21 +57,27 @@ dofile("C:\\Program Files\\World of Warcraft\\Interface\\AddOns\\Ace\\AceHook.lu
 dofile("C:\\Program Files\\World of Warcraft\\Interface\\AddOns\\Ace\\AceModule.lua")
 dofile("C:\\Program Files\\World of Warcraft\\Interface\\AddOns\\GuildAds\\data\\GuildAdsDB.lua");
 dofile("C:\\Program Files\\World of Warcraft\\Interface\\AddOns\\GuildAds\\data\\GuildAdsDataType.lua");
+dofile("C:\\Program Files\\World of Warcraft\\Interface\\AddOns\\GuildAds\\data\\GuildAdsTableDataType.lua");
 dofile("C:\\Program Files\\World of Warcraft\\Interface\\AddOns\\GuildAds\\data\\datatypes\\GuildAdsTradeNeedData.lua");
 dofile("C:\\Program Files\\World of Warcraft\\Interface\\AddOns\\GuildAds\\data\\datatypes\\GuildAdsTradeOfferData.lua");
+dofile("C:\\Program Files\\World of Warcraft\\Interface\\AddOns\\GuildAds\\data\\datatypes\\GuildAdsTradeSkillData.lua");
 dofile("C:\\Program Files\\World of Warcraft\\Interface\\AddOns\\GuildAds\\data\\datatypes\\GuildAdsSkillData.lua");
 dofile("C:\\Program Files\\World of Warcraft\\WTF\\Account\\ALFINWOW\\SavedVariables\\GuildAds.lua");
 
 GuildAds = {
-	playerName = "Ingird";
+	playerName = config.playerName;
 	channelName = "GuildAdsInTartifletteWeTrust";
-	realmName = "Ner'zhul";
-	factionName = "Horde";
+	realmName = config.realmName;
+	factionName = config.faction;
 	db = AceDatabase:new("GuildAdsDatabase");
 }
 
 GuildAds.db:Initialize();
 GuildAdsDB:Initialize();
+
+--
+-- Tests
+--
 
 --[[
 print ("Need (me) --------------");
@@ -57,12 +101,37 @@ for _, item, playerName, data in GuildAdsDB.channel[GuildAds.channelName].TradeO
 end
 ]]
 
---~ local players = GuildAdsDB.channel[GuildAds.channelName]:getPlayers();
-local players = GuildAdsDB.channel[GuildAds.channelName].db.Players;
-for i,j in pairs(players) do
-	print(tostring(i)..","..tostring(j));
+function PrintUpdate(dataType, playerName, fromRevision)
+	local currentRevision = dataType:getRevision(playerName);
+	GuildAds_ChatDebug(GA_DEBUG_PROTOCOL,"currentRevision="..currentRevision);
+	local t = {};
+	local n = {};
+	local newEntries = 0;
+	-- send new entries >r1
+	for id, _, data, revision in dataType:iterator(playerName) do
+		if (revision>fromRevision) then
+			newEntries = newEntries + 1;
+			tinsert(n, { r=revision, i=id, d=data});
+		else
+			table.insert(t, revision);
+		end
+	end
+	
+	table.sort(n, function(a,b) return a.r<b.r end);
+	local f = function(k, v)
+		print("  -N["..v.r.."]="..tostring(v.i).."/"..tostring(v.d));
+	end;
+	table.foreach(n, f);
+
+	if currentRevision-fromRevision~=newEntries then
+		-- idealement : 1-10, 12-15, 17-30 au lieu de la liste complete
+		table.sort(t);
+		local f = function(k, v)
+		end;
+		table.foreach(t, f);
+	end
 end
 
-for id, playerName , data in GuildAdsSkillDataType:iterator(nil, 12) do
-	print("  - add("..tostring(playerName)..","..tostring(id)..")");
-end
+print(GuildAdsDB.profile.TradeSkill:getRevision("Zarkan"));
+
+PrintUpdate(GuildAdsDB.profile.TradeSkill, "Zarkan", 245);
