f71ab3bb57e77e3560e35b93619ed0cdfe0e49f2
flament <flament@eb54f2c9-7f0f-0410-ba04-aaec33d8da6a>
2006-04-27 22:30:19 +0000

    Cleaner code to do tests
    git-svn-id: svn://svn.code.sf.net/p/guildads/code@91 eb54f2c9-7f0f-0410-ba04-aaec33d8da6a

---
+++ GuildAds/data/testDB.lua
@@ -0,0 +1,77 @@
+-- defined config table : playerName, realmName, faction, channelName, wowPath, guildAdsDatabaseFile
+dofile("testDBConfig.lua");
+
+--
+-- About WOW
+--
+SlashCmdList = {}
+tinsert = table.insert;
+getn = table.getn;
+
+getglobal = function(name)
+	return _G[name];
+end
+
+setglobal = function(name, value)
+	_G[name] = value;
+end
+
+GetCVar = function(name)
+	if name=="realmName" then
+		return config.realmName;
+	end
+end
+
+UnitName = function(name)
+	if name=="player" then
+		return config.playerName;
+	end
+end
+
+--
+-- About Ace
+--
+AceEventFrame = {
+	RegisterEvent = function(event)
+	end;
+}
+
+dofile(config.wowPath.."Interface\\AddOns\\Ace\\Ace.lua")
+dofile(config.wowPath.."Interface\\AddOns\\Ace\\AceDB.lua")
+dofile(config.wowPath.."Interface\\AddOns\\Ace\\AceData.lua")
+dofile(config.wowPath.."Interface\\AddOns\\Ace\\AceEvent.lua")
+dofile(config.wowPath.."Interface\\AddOns\\Ace\\AceHook.lua")
+dofile(config.wowPath.."Interface\\AddOns\\Ace\\AceModule.lua")
+
+--
+-- About GuildAds
+--
+GUILDADS_VERSION = 200;
+
+GuildAdsTask = {
+	AddNamedSchedule = function(self, n, t, r, c, f, ...)
+	end
+}
+
+GuildAds_ChatDebug = function(t, m)
+end
+
+dofile(config.guildAdsDatabaseFile);
+dofile(config.wowPath.."Interface\\AddOns\\GuildAds\\data\\GuildAdsDB.lua");
+dofile(config.wowPath.."Interface\\AddOns\\GuildAds\\data\\GuildAdsDataType.lua");
+dofile(config.wowPath.."Interface\\AddOns\\GuildAds\\data\\GuildAdsTableDataType.lua");
+dofile(config.wowPath.."Interface\\AddOns\\GuildAds\\data\\datatypes\\GuildAdsTradeNeedData.lua");
+dofile(config.wowPath.."Interface\\AddOns\\GuildAds\\data\\datatypes\\GuildAdsTradeOfferData.lua");
+dofile(config.wowPath.."Interface\\AddOns\\GuildAds\\data\\datatypes\\GuildAdsTradeSkillData.lua");
+dofile(config.wowPath.."Interface\\AddOns\\GuildAds\\data\\datatypes\\GuildAdsSkillData.lua");
+
+GuildAds = {
+	playerName = config.playerName;
+	channelName = config.channelName;
+	realmName = config.realmName;
+	factionName = config.faction;
+	db = AceDatabase:new("GuildAdsDatabase");
+}
+
+GuildAds.db:Initialize();
+GuildAdsDB:Initialize();
