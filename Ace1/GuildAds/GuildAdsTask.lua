42f188b217c6fbf956c531d6df2572a6de1a4a2f
flament <flament@eb54f2c9-7f0f-0410-ba04-aaec33d8da6a>
2007-11-24 15:31:01 +0000

    GuildAdsTask.lua based on Dongle (version 1.1)
    git-svn-id: svn://svn.code.sf.net/p/guildads/code@237 eb54f2c9-7f0f-0410-ba04-aaec33d8da6a

--- Ace1/GuildAds/GuildAdsTask.lua
+++ Ace1/GuildAds/GuildAdsTask.lua
@@ -8,82 +8,171 @@
 -- Licence: GPL version 2 (General Public License)
 ----------------------------------------------------------------------------------
 
--- based on TimeXCore.lua, revision 637 from https://opensvn.csie.org/traccgi/Ace/trac.cgi/browser/trunk/Timex/Core
+-- based on http://dongle.googlecode.com/svn/branches/Dongle-1.1/Dongle.lua
 
-GuildAdsTask = { };
+local _G = _G
+local table_insert = _G.table.insert
+local table_remove = _G.table.remove
+local table_concat = _G.table.concat
+local unpack = _G.unpack
+local pairs = _G.pairs
+local next = _G.next
 
-local emptyArray = {};
-local frame;
-local timerDB = {};
+GuildAdsTask = { }
 
-local OnUpdate = function(this, elapsed)
-	for k, v in pairs(timerDB) do
-		v.e = v.e + elapsed
-		if v.t and v.e >= v.t then
-			v.e = 0
-			if v.c then
-				v.c = v.c - 1
-				if v.c <= 0 then
-					tremove(timerDB, k)
-				end
-			elseif not v.r then
-				tremove(timerDB, k)
-			end
-			if v.f then 
-				v.f(unpack(v.a or emptyArray))
-			end
+local emptyArray = {}
+local frame
+local timers = {}
+local heap = {}
+local localtime = 0
+
+local function HeapSwap(i1, i2)
+	heap[i1], heap[i2] = heap[i2], heap[i1]
+end
+
+local function HeapBubbleUp(index)
+	while index > 1 do
+		local parentIndex = math.floor(index / 2)
+		if heap[index].timeToFire < heap[parentIndex].timeToFire then
+			HeapSwap(index, parentIndex)
+			index = parentIndex
+		else
+			break
 		end
-	end
-	if #timerDB == 0 then
-		frame:Hide()
 	end
 end
 
-function GuildAdsTask:DB()
-	return timerDB;
+local function HeapBubbleDown(index)
+	while 2 * index <= heap.lastIndex do
+		local leftIndex = 2 * index
+		local rightIndex = leftIndex + 1
+		local current = heap[index]
+		local leftChild = heap[leftIndex]
+		local rightChild = heap[rightIndex]
+
+		if not rightChild then
+			if leftChild.timeToFire < current.timeToFire then
+				HeapSwap(index, leftIndex)
+				index = leftIndex
+			else
+				break
+			end
+		else
+			if leftChild.timeToFire < current.timeToFire or
+			   rightChild.timeToFire < current.timeToFire then
+				if leftChild.timeToFire < rightChild.timeToFire then
+					HeapSwap(index, leftIndex)
+					index = leftIndex
+				else
+					HeapSwap(index, rightIndex)
+					index = rightIndex
+				end
+			else
+				break
+			end
+		end
+	end
+end
+
+local function OnUpdate(frame, elapsed)
+	localtime = localtime + elapsed;
+	local schedule = heap[1]
+	while schedule and schedule.timeToFire < localtime do
+		-- if there is lag, get back in time, and exit for now
+		if elapsed>0.5 then
+			GuildAds_ChatDebug(GA_DEBUG_GLOBAL, "lag, delay for scheduled tasks")
+			localtime = localtime - elapsed
+			return
+		end
+		-- 
+		if schedule.cancelled then
+			HeapSwap(1, heap.lastIndex)
+			heap[heap.lastIndex] = nil
+			heap.lastIndex = heap.lastIndex - 1
+			HeapBubbleDown(1)
+		else
+			schedule.func(unpack(schedule.args or emptyArray))
+			
+			if schedule.repeating then
+				schedule.timeToFire = schedule.timeToFire + schedule.repeating
+				HeapBubbleDown(1)
+			else
+				HeapSwap(1, heap.lastIndex)
+				heap[heap.lastIndex] = nil
+				heap.lastIndex = heap.lastIndex - 1
+				HeapBubbleDown(1)
+				timers[schedule.name] = nil
+			end
+			
+		end
+		schedule = heap[1]
+	end
+	if not schedule then frame:Hide() end
+end
+
+GuildAdsTask.HeapSwap = HeapSwap
+GuildAdsTask.HeapBubbleUp = HeapBubbleUp
+GuildAdsTask.HeapBubbleDown = HeapBubbleDown
+
+function GuildAdsTask:AddNamedSchedule(name, t, r, count, func, ...)
+	assert(c==nil, "c~=nil not implemented");
+	self:ScheduleTimer(name, func, t, ...)
+	if r then
+		timers[name].repeating = t
+	end
+end
+
+function GuildAdsTask:ScheduleTimer(name, func, delay, ...)
+	if GuildAdsTask:NamedScheduleCheck(name) then
+		GuildAdsTask:DeleteNamedSchedule(name)
+	end
+
+	local schedule = {}
+	timers[name] = schedule
+	schedule.timeToFire = localtime + delay
+	schedule.func = func
+	schedule.name = name
+	if select('#', ...) ~= 0 then
+		schedule.args = { ... }
+	end
+
+	if heap.lastIndex then
+		heap.lastIndex = heap.lastIndex + 1
+	else
+		heap.lastIndex = 1
+	end
+	heap[heap.lastIndex] = schedule
+	HeapBubbleUp(heap.lastIndex)
+	if not frame:IsShown() then
+		frame:Show()
+	end
+end
+
+function GuildAdsTask:NamedScheduleCheck(name)
+	local schedule = timers[name]
+	if schedule then
+		return true, schedule.timeToFire - localtime
+	else
+		return false
+	end
+end
+
+function GuildAdsTask:DeleteNamedSchedule(name)
+	local schedule = timers[name]
+	if not schedule then return end
+	schedule.cancelled = true
+	timers[name] = nil
+end
+
+function GuildAdsTask:GetTasks()
+	return timers, heap;
+end
+
+function GuildAdsTask:GetOnUpdate()
+	return OnUpdate;
 end
 
 function GuildAdsTask:Initialize()
 	frame = CreateFrame("Frame")
 	frame:SetScript("OnUpdate", OnUpdate)
 end
-
-function GuildAdsTask:NamedScheduleCheck(n, r)
-    for k, v in pairs(timerDB) do
-		if v.n == n then
-			r = r and (v.e or 0) or TRUE
-			return r
-		end
-	end
-end
-
-function GuildAdsTask:AddNamedSchedule(n, t, r, c, f, ...)
-	if not n and not t then return end
-    self:DeleteNamedSchedule(n)
-    tinsert(timerDB, {
-		n = n or this:GetName(),
-        t = tonumber(t),
-        r = r,
-        c = tonumber(c),
-        e = 0,
-        f = f,
-		a = { select(1, ...) }
-	})
-	frame:Show()
-end
-
-function GuildAdsTask:DeleteNamedSchedule(n)
-	for k, v in pairs(timerDB) do
-		if v.n == n then tremove(timerDB, k) end
-	end
-	if not next(timerDB) then
-		frame:Hide()
-	end
-end
-
-function GuildAdsTask:ChangeDuration(n, t)
-	for k, v in pairs(timerDB) do
-		if v.n == n then v.t = t or v.t end
-	end
-end
-
