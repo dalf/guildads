947e1b5ad2e6adf21fd373a260af45d7eba3c9c9
galmok <galmok@eb54f2c9-7f0f-0410-ba04-aaec33d8da6a>
2009-04-26 12:15:22 +0000

    Clicking on quests now expands to show individual difficulty and completion status. Overall (collapsed) display does not show completion state anymore. Table reuse implemented.
    git-svn-id: svn://svn.code.sf.net/p/guildads/code@454 eb54f2c9-7f0f-0410-ba04-aaec33d8da6a

--- Ace3/GuildAds/ui/quest/GuildAdsQuestFrame.lua
+++ Ace3/GuildAds/ui/quest/GuildAdsQuestFrame.lua
@@ -8,6 +8,11 @@
 -- Licence: GPL version 2 (General Public License)
 ----------------------------------------------------------------------------------
 
+local new = GuildAds.new
+local new_kv = GuildAds.new_kv
+local del = GuildAds.del
+local deepDel = GuildAds.deepDel
+
 GuildAdsQuest = {
 	metaInformations = { 
 		name = "Quest",
@@ -25,7 +30,7 @@ GuildAdsQuest = {
 
 	GUILDADS_NUM_QUEST_BUTTONS = 27;
 	GUILDADS_QUESTBUTTONSIZEY = 16;
-		
+	
 	onInit = function()
 	end;
 	
@@ -106,33 +111,67 @@ GuildAdsQuest = {
 			-- init
 			local i = 1;
 			local j = i + offset;
-			local currentQuest, currentSelection, button;
-			
+			local k = 0;
+			local currentQuest, button, currentIndex, numPlayers;
+			local currentSelection = GuildAdsQuest.currentSelectedQuestId;
+			if currentSelection then
+				currentIndex = GuildAdsQuest.getLinearIndex(linear, currentSelection)
+				if type(linear[currentIndex].p) == "string" then
+					numPlayers = 1
+				else
+					numPlayers = #linear[currentIndex].p
+				end
+				linearSize = linearSize + numPlayers
+				belowCurrent = currentIndex + numPlayers
+			end
+
 			-- for each buttons
 			while (i <= GuildAdsQuest.GUILDADS_NUM_QUEST_BUTTONS) do
 				button = getglobal("GuildAdsQuestButton"..i);
 				
 				currentQuest = linear[j];
-				j = j +1;
+				if currentSelection then
+					if j <= currentIndex then
+						k = 0
+						currentQuest = linear[j];
+					elseif j<= belowCurrent then
+						k = j - currentIndex
+						currentQuest = linear[j-k];
+					else
+						currentQuest = linear[j - numPlayers];
+						k = 0
+					end
+				end
+				j=j+1
 				
 				if (currentQuest ~= nil) then
 					button.questId = currentQuest.id;
 					button.data = currentQuest;
 					
-					currentSelection = GuildAdsQuest.currentSelectedQuestId;
-					GuildAdsQuest.questLineButton.update(button, currentSelection==currentQuest.id, currentQuest);
+					GuildAdsQuest.questLineButton.update(button, currentSelection==currentQuest.id, currentQuest, k);
 					button:Show();
 				else
 					button.questId = nil;
 					button.data = nil;
 					button:Hide();
 				end;
+				
 				i = i+1;
 			end;
 			FauxScrollFrame_Update(GuildAdsQuestScrollFrame, linearSize, GuildAdsQuest.GUILDADS_NUM_QUEST_BUTTONS, GuildAdsQuest.GUILDADS_QUESTBUTTONSIZEY);
 		--end;
 	end;
-			
+	
+	getLinearIndex= function(linear, questID)
+		if questID then
+			for idx, data in pairs(linear) do
+				if data.id == questID then
+					return idx
+				end
+			end
+		end
+	end;
+	
 	questLineButton = {
 		
 		t = {};
@@ -158,7 +197,7 @@ GuildAdsQuest = {
 			end
 		end;
 
-		update = function(button, selected, quest)
+		update = function(button, selected, quest, subPlayer)
 			local buttonName= button:GetName();
 			
 			local groupField = getglobal(buttonName.."Group");
@@ -170,26 +209,67 @@ GuildAdsQuest = {
 
 			local datatype = GuildAdsDB.profile.Quest;
 
-			if selected then
+			local expanded
+			if subPlayer > 0 and subPlayer <= #quest.p then
+				expanded = true
+			end
+
+			if selected and not expanded then
 				button:LockHighlight();
 			else
 				button:UnlockHighlight();
 			end
-			
-			groupField:SetText(quest.g);
+			if expanded then
+				groupField:SetText("   "..quest.g);
+			else
+				groupField:SetText(quest.g);
+			end
 			local color = QuestDifficultyColor["header"];
 			groupField:SetTextColor(color.r, color.g, color.b);
 			groupField:Show();
 			
-			color = GetDifficultyColor(quest.l);
+			local dcolor = GetDifficultyColor(quest.l);
+			local color = new_kv("r", dcolor.r, "g", dcolor.g, "b", dcolor.b)
+			if expanded and quest.c then
+				local c = quest.c
+				if type(c)=="table" then
+					c = c[quest.p[subPlayer]]
+				end
+				local start,_,a,r,g,b=string.find(c, "(..)(..)(..)(..)")
+				if start then
+					color.r = tonumber(r,16)/255
+					color.g = tonumber(g,16)/255
+					color.b = tonumber(b,16)/255
+				end
+			end
 			nameField:SetTextColor(color.r, color.g, color.b);
 			nameField:SetText(quest.n);
 			nameField:Show();
 			GuildAdsQuestTagDummyText:SetText(quest.n);
 			
 			-- questTag
-			local isDaily = bit.band(quest.s, 4)==4 and true or false
-			local isComplete = bit.band(quest.s, 3) - 1
+			local isDaily
+			if type(quest.s) ~= "table" then
+				isDaily = quest.s
+			else
+				if expanded then
+					isDaily = quest.s[quest.p[subPlayer]]
+				else
+					isDaily = select(2,next(quest.s)) -- just pick the first one
+				end
+			end
+			isDaily = bit.band(isDaily, 4)==4 and true or false
+			
+			local isComplete
+			if expanded then
+				if type(quest.s) ~= "table" then
+					isComplete = bit.band(quest.s, 3) - 1
+				else
+					isComplete = bit.band(quest.s[quest.p[subPlayer]], 3) - 1
+				end
+			else
+				isComplete = 0
+			end
 			local questTag = GuildAdsDB.profile.Quest:GetVerboseQuestTag(quest.r)
 			if ( isComplete and isComplete < 0 ) then
 				questTag = FAILED;
@@ -226,9 +306,15 @@ GuildAdsQuest = {
 			
 		
 			-- online/offline highlight
-			if type(quest.p)=="string" then
-				ownerColor = GuildAdsUITools:GetPlayerColor(quest.p)
-				playerField:SetText(quest.p);
+			local player = quest.p
+			if expanded then
+				if type(player)~="string" then
+					player = quest.p[subPlayer]
+				end
+			end
+			if type(player)=="string" then
+				ownerColor = GuildAdsUITools:GetPlayerColor(player)
+				playerField:SetText(player);
 			else
 				ga_table_erase(GuildAdsQuest.questLineButton.t);
 				local online, accountOnline, atLeastOneOnline, atLeastOneOnlineAccount;
@@ -255,6 +341,8 @@ GuildAdsQuest = {
 						
 			levelField:SetText(quest.l)
 			levelField:Show()
+			
+			del(color)
 		end;
 		
 		onEnter = function(self, button)
@@ -273,7 +361,6 @@ GuildAdsQuest = {
 	--
 	---------------------------------------------------------------------------------	
 	contextMenu = {
-		currentItem=false;
 		
 		onLoad = function()
 			HideDropDownMenu(1);
@@ -290,7 +377,7 @@ GuildAdsQuest = {
 		
 		addPlayer = function(playerName)
 			local _, color = GuildAdsUITools:GetPlayerColor(playerName);
-			local info = { };
+			local info = new();
 			info.text =  color..playerName.."|r";
 			info.value = playerName;
 			info.notCheckable = 1;
@@ -299,13 +386,14 @@ GuildAdsQuest = {
 			-- info.func = ToggleDropDownMenu;
 			-- info.arg1 = 2;
 			-- info.arg2 = playerName
-			UIDropDownMenu_AddButton(info, 1);			
+			UIDropDownMenu_AddButton(info, 1);
+			del(info)
 		end;
 		
 		initialize = function(frame, level)
 			if level==1 then
 				if type(GuildAdsQuestContextMenu.data.p)=="string" then
-					GuildAdsPlayerMenu.initialize(GuildAdsQuest.currentPlayerName, 1);
+					GuildAdsPlayerMenu.initialize(GuildAdsQuestContextMenu.data.p, 1);
 				elseif type(GuildAdsQuestContextMenu.data.p)=="table" then
 					for _, name in ipairs(GuildAdsQuestContextMenu.data.p) do
 						GuildAdsQuest.contextMenu.addPlayer(name);
@@ -322,7 +410,7 @@ GuildAdsQuest = {
 		cacheTree = nil;
 		
 		resetCache = function()
-			GuildAdsQuest.data.cache = nil;
+			GuildAdsQuest.data.cacheReset = true
 		end;
 		
 		get = function(updateData)
@@ -335,31 +423,48 @@ GuildAdsQuest = {
 		end;
 		
 		get2 = function(updateData)
-			if GuildAdsQuest.data.cache==nil or updateData==true then
+			if GuildAdsQuest.data.cache==nil or updateData==true or GuildAdsQuest.data.cacheReset==true then
+				GuildAdsQuest.data.cacheReset = false
 				local players = GuildAdsDB.channel[GuildAds.channelName]:getPlayers();
 				
-				-- create linear list of all quests
-				local workingTable = {}
+				-- reuse table to avoid memory fragmentation
+				deepDel(GuildAdsQuest.data.cache)
+				workingTable = new()
 				
+				-- create linear list of all quests				
 				local datatype = GuildAdsDB.profile.Quest;
 				for playerName in pairs(players) do
 					for questId, _, data in datatype:iterator(playerName, nil) do
 						--GuildAdsQuest.debug("playerName = "..playerName.."   questId = "..questId);
 						if workingTable[questId] then
+							if type(workingTable[questId].s) ~= "table" then
+								workingTable[questId].s = new_kv(workingTable[questId].p, workingTable[questId].s )
+							end
+							workingTable[questId].s[playerName] = data.s
+							
+							if type(workingTable[questId].c) ~= "table" then
+								workingTable[questId].c = new_kv(workingTable[questId].p, workingTable[questId].c)
+							end
+							workingTable[questId].c[playerName] = data.c
+
+							if type(workingTable[questId].p) ~= "table" then
+								workingTable[questId].p = new(workingTable[questId].p)
+							end
 							tinsert(workingTable[questId].p, playerName)
 						else
-							workingTable[questId] = { id=questId, g=data.g, n=data.n, r=data.r, l=data.l, c=data.c, s=data.s, p={ playerName } }
+							workingTable[questId] = new_kv("id", questId, "g", data.g, "n", data.n, "r", data.r, "l", data.l, "c", data.c, "s", data.s, "p", playerName)
 						end
-						--table.insert(workingTable, { id=questId, g=data.g, n=data.n, r=data.r, l=data.l, c=data.c, s=data.s, p=playerName })
 					end
 				end
 				-- create sortable list
-				local tmp = {}
+				local tmp = new()
 				for questId, data in pairs(workingTable) do
-					table.sort(data.p, GuildAdsQuest.sortData.predicateFunctions.quester);
+					if type(workingTable[questId].p) == "table" then
+						table.sort(data.p, GuildAdsQuest.sortData.predicateFunctions.quester);
+					end
 					tinsert(tmp, data)
 				end
-				workingTable = nil -- free memory
+				del(workingTable) -- free memory
 				GuildAdsQuest.data.cache = tmp;
 				GuildAdsQuest.sortData.doIt(GuildAdsQuest.data.cache);
 			end
