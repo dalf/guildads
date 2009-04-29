9316cfbd314198b36bf77eb70c4ae58780cdeaac
galmok <galmok@eb54f2c9-7f0f-0410-ba04-aaec33d8da6a>
2009-04-29 18:51:39 +0000

    Quest color in link wasn't done correctly. Fixed.
    git-svn-id: svn://svn.code.sf.net/p/guildads/code@457 eb54f2c9-7f0f-0410-ba04-aaec33d8da6a

--- Ace3/GuildAds/ui/quest/GuildAdsQuestFrame.lua
+++ Ace3/GuildAds/ui/quest/GuildAdsQuestFrame.lua
@@ -187,7 +187,9 @@ GuildAdsQuest = {
 				end
 				if button=="LeftButton" and IsShiftKeyDown() and ChatFrameEditBox:IsVisible() then
 					local questLink = "quest:"..self.questId..":"..self.data.l
-					local questLink = "|c"..self.data.c.."|H"..questLink.."|h["..self.data.n.."]|h|r"
+					local color = GetDifficultyColor(self.data.l);
+					color = string.format("ff%2x%2x%2x",color.r*255,color.g*255,color.b*255)
+					local questLink = "|c"..color.."|H"..questLink.."|h["..self.data.n.."]|h|r"
 					ChatFrameEditBox:Insert(questLink);
 				end
 				if button == "RightButton" then
